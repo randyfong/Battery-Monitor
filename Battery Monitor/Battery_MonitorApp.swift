c6d0448a8425e5c801cd61fce4a8ec57d3ce9ba1
Randy Fong <randyfong@aol.com>
2023-04-27 10:33:46 -0700

    Initial Commit

---
+++ Battery Monitor/Battery_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Battery_MonitorApp.swift
+//  Battery Monitor
+//
+//  Created by Randy Fong on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Battery_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
